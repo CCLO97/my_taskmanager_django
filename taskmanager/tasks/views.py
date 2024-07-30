6367424ed604f9ea0e25f5e8e8d70fc177a7bb85
CCLO97 <cristianclondonoo@gmail.com>
2024-07-29 20:56:17 -0500

    Añadir archivos

--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from .models import Task
+from .forms import TaskForm
 
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
+def task_create(request):
+    if request.methhod == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form - TaskForm()
+    return render(request, 'tasks/task_form.html', {'form' : form})
